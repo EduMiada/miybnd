9aef1ba1ccdf5228406c6af164d2f219e47b27f0
Miyada2000 <miyada@hotmail.com>
2016-01-22 15:59:54 -0200

    a

--- config/env/production.js
+++ config/env/production.js
@@ -41,7 +41,7 @@ module.exports = {
 		js: 'public/dist/application.min.js'
 
 	},
-	spotify: {
+	/*spotify: {
         clientID: '5063d7fc578d4b928e96e050790860c9' || 'APP_ID',
         clientSecret: 'f6f4758ea04942668385ab0d4953e014' || 'APP_SECRET',
         callbackURL: "/auth/spotify/callback"
@@ -50,7 +50,21 @@ module.exports = {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
 		clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
 		callbackURL: '/auth/facebook/callback'
+	},*/
+    
+    spotify: {
+        clientID: '5063d7fc578d4b928e96e050790860c9' || 'APP_ID',
+        clientSecret: 'f6f4758ea04942668385ab0d4953e014' || 'APP_SECRET',
+        callbackURL: "/auth/spotify/callback"
+    },
+	facebook: {
+		clientID: '162584200776112' || 'APP_ID',
+		clientSecret: '99cbd345ef5002f60e56e89b9d5eb4a9' || 'APP_SECRET',
+		callbackURL: '/auth/facebook/callback'
 	},
+    
+    
+    
 	twitter: {
 		clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
 		clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
